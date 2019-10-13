acd2692bcf4a005cf29bda327f714e80871402ef
Kateryna Melnykova <forkatemelnikova@gmail.com>
2019-10-13 09:58:02 +0700

    First working version

--- mergingHW/manage.py
+++ mergingHW/manage.py
@@ -1,6 +1,8 @@
 import os
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for, request, send_file
+
+from merge_files import *
 
 app = Flask(__name__)
 
@@ -8,14 +10,29 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/select_files')
 def select_files():
-    return "hello world"
+    return render_template('select_files.html')
 
 
-@app.route('/order_of_files')
+@app.route('/order_of_files', methods=['GET', 'POST'])
 def order_of_files():
-    return "Determine the order of concatenation"
+    filenames = get_all_files()
+    return render_template('order_of_files.html', filenames=filenames)
 
 
-@app.route('/concat_and_order')
+@app.route('/concat_and_order', methods=['POST'])
 def concat_and_order():
-    return "here is your .def file"
+    filenames = request.form['filenames']
+    filenames = filenames.split('/')
+    # TODO: read and implement ordering
+    concat_and_enumerate(filenames)
+    return redirect(url_for('output'))
+
+
+@app.route('/output')
+def output():
+    return render_template('output.html')
+
+
+@app.route('/see_file')
+def see_file():
+    return send_file('./output/output.def', as_attachment=True)
--- mergingHW/merge_files.py
+++ mergingHW/merge_files.py
@@ -1,21 +1,45 @@
 import os
+from typing import List
 
 # Q: How to find files on user PC?
-FOLDER_NAME = '/storage/'
+FOLDER_NAME = './storage/'
 
-filenames = []
-# get all files whose extension is .def
-for file in os.listdir(FOLDER_NAME):
-    if file.endswith(".def"):
-        filenames.append(file)
 
-print(f'Found files {filenames}')
+def get_all_files(folder: str = FOLDER_NAME):
+    filenames = []
+    # get all files whose extension is .def
+    for file in os.listdir(folder):
+        if file.endswith(".def"):
+            filenames.append(file)
+    filenames = sorted(filenames)
+    print(f'Found files {filenames}')
+    return filenames
 
-# concatenate all files
-output = open('/output/output.def', 'w')
-for file in filenames:
-    with open(os.path.join(FOLDER_NAME, file)) as hw:
+
+def concat_and_enumerate(filenames: List[str], folder=FOLDER_NAME):
+    """
+    concatenate all files and include enumeration of problems
+    Returns file with name 'output.def'
+    :param filenames: list of all files to concatenate
+    :param folder: the folder where all files are located
+    """
+    problem_id = 1
+    output = open('./output/output.def', 'w+')
+    with open(os.path.join(folder, filenames[0])) as hw:
         output.write(hw.read())
 
-output.close()
+    for file in filenames[1:]:
+        with open(os.path.join(folder, file)) as hw:
+            header = True
+            for line in hw:
+                if not header:
+                    if line.startswith('problem_id = '):
+                        line = 'problem_id = ' + str(problem_id) + '\n'
+                        problem_id += 1
+                    output.write(line)
+
+                if line.startswith('problemListV2'):
+                    header = False
+
+    output.close()
 
